22961e97dfc448e9290b1ed4ae0e5ccae99cc78a
brisariesco <riescobrisa99@gmail.com>
2024-11-29 20:57:08 -0300

    Add files via upload

---
+++ ejercicio 3.js
@@ -0,0 +1,7 @@
+function areaRectangulo(altura,ancho){
+
+    let area =(altura*ancho);
+    return area;
+}
+let areafinal= areaRectangulo(2,3);
+console.log(areafinal);
---
+++ ejercicio 4.js
@@ -0,0 +1,12 @@
+
+ function calcularPrecioConDescuento(precioOriginal, descuento)
+ { 
+let descuento2= (precioOriginal*descuento)/100;   
+
+return descuento2;
+ }
+ let preciofinal= calcularPrecioConDescuento(800,30);
+console.log(preciofinal);
+
+
+
